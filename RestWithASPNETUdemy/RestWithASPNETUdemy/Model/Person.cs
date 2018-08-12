d90cc82ba1bb4f0417e05286bb384a0424c9ccf1
gleidsonguilherme <gleidson@gleidson.com.br>
2018-08-12 19:27:39 -0300

    teste

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string PrimeiroNome { get; set; }
+        public string UltimoNome { get; set; }
+        public string Endereco { get; set; }
+        public string Sexo { get; set; }
+    }
+}
